d0e6d36f2b19d96fe859c9406056fc4821e22c95
Swathi P <swathip@Swathis-MacBook-Air.local>
2024-11-10 14:10:31 +0530

    added user details api

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 
 //sample api
-app.get('/api/get', (req,res)=>{
-    res.status(200).send({message:"Hello from node js in AWS"})
+app.get('/api/get', (req, res) => {
+    res.status(200).send({ message: "Hello from node js in AWS" })
 })
 
-app.listen(8000, ()=>{
+app.get('/api/get_user_details', (req, res) => {
+    res.send({
+        user: {
+            name: "Swathi",
+            contact: "1234"
+        }
+    })
+})
+
+app.listen(8000, () => {
     console.log("server is listening to 8000")
 })
