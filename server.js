85a528a15cb6975cd75bba9b77a4ebfe407831b4
Swathi P <swathip@Swathis-MacBook-Air.local>
2024-11-10 14:43:53 +0530

    added env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+require("dotenv").config
 //sample api
 app.get('/api/get', (req, res) => {
     res.status(200).send({ message: "Hello from node js in AWS" })
@@ -11,10 +11,11 @@ app.get('/api/get_user_details', (req, res) => {
         user: {
             name: "Swathi",
             contact: "1234"
-        }
+        },
+        env: process.env.NAME
     })
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log("server is listening to 8000")
 })
